782c8dddef759b6b8edb427506e4a9578aa1417d
xiong <313398439@qq.com>
2018-05-07 23:49:23 +0800

    algorithms/35. Search Insert Position/Solution2.java

--- algorithms/35. Search Insert Position/Solution2.java
+++ algorithms/35. Search Insert Position/Solution2.java
@@ -23,14 +23,6 @@ Output: 0
 
 /*
 二分查找答案的标准写法
-(1) At this point, low > high. That is, low >= high+1
-(2) From the invariant, we know that the index is between [low, high+1], 
-so low <= high+1. Follwing from (1), now we know low == high+1.
-(3) Following from (2), the index is between [low, high+1] = [low, low], 
-which means that low is the desired index
-
-Therefore, we return low as the answer. 
-You can also return high+1 as the result, since low == high+1
 */
 
 class Solution2 {
@@ -41,12 +33,22 @@ class Solution2 {
         while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (nums[mid] < target)
+            if (target > nums[mid])
                 low = mid + 1;
             else
                 high = mid - 1;
         }
 
+        /*
+        (1) At this point, low > high. That is, low >= high+1
+        (2) From the invariant, we know that the index is between [low, high+1], 
+        so low <= high+1. Follwing from (1), now we know low == high+1.
+        (3) Following from (2), the index is between [low, high+1] = [low, low], 
+        which means that low is the desired index
+
+        Therefore, we return low as the answer. 
+        You can also return high+1 as the result, since low == high+1
+        */
         return low;
     }
 }
