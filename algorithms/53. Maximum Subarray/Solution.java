7c8dbbe16010f7140a768e675b8ea15ce3097506
xiong <313398439@qq.com>
2018-05-11 23:36:19 +0800

    algorithms/53. Maximum Subarray/Solution.java

--- algorithms/53. Maximum Subarray/Solution.java
+++ algorithms/53. Maximum Subarray/Solution.java
@@ -13,6 +13,15 @@ If you have figured out the O(n) solution, try coding another solution using the
 
 class Solution {
     public int maxSubArray(int[] nums) {
-        
+        int max = nums[0];
+        int total = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total = total + nums[i];
+            if (total > max)
+                max = total;
+            if (total < 0)
+                total = 0;            
+        }
+        return max;
     }
 }
