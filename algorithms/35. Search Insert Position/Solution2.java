8e0a110f898afec43b64014ae83ba2b0b0bbe2da
xiong <313398439@qq.com>
2018-07-01 22:48:50 +0800

    标准二分 完善

--- algorithms/35. Search Insert Position/Solution2.java
+++ algorithms/35. Search Insert Position/Solution2.java
@@ -26,29 +26,51 @@ Output: 0
 */
 
 class Solution2 {
+     /**
+     * 情况一：target在数组中存在
+     * return mid 返回 target在数组中实际的位置
+     * 
+     * 情况二：target在数组中不存在
+     * 
+     * 1. 若nums[i] < target < nums[i+1]
+     * ---倒数第二轮循环---
+     * 进循环前：low=i high=i+1
+     * 循环体执行后：mid=i low=mid+1=i+1 high=i+1
+     * ----最后一轮循环----
+     * 进循环前：low=i+1 high=i+1
+     * 循环体执行后：mid=i+1 low=mid+1=i+2 high=i+1
+     * ------退出循环------
+     * return low 返回i+1 为target应该插入的位置
+     * 
+     * 2. 若target < nums[0]
+     * ----最后一轮循环----
+     * 进循环前：low=0 high=1
+     * 循环体执行后：mid=0 high=mid-1=-1 low=0
+     * ------退出循环------
+     * return low 返回0 为target应该插入的位置
+     *
+     * 3. 若target > nums[nums.length-1]
+     * ---倒数第二轮循环---
+     * 进循环前：low=nums.length-2 high=nums.length-1
+     * 循环体执行后：mid=nums.length-2 low=mid+1=nums.length-1 high=nums.length-1
+     * ----最后一轮循环----
+     * 进循环前：low=nums.length-1 high=nums.length-1
+     * 循环体执行后：mid=nums.length-1 low=mid+1=nums.length high=nums.length-1
+     * ------退出循环------
+     * return low 返回nums.length 为target应该插入的位置
+     */
     public int searchInsert(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
-
-        // Invariant: the desired index is between [low, high+1]
+        int low = 0, high = nums.length - 1, mid = (low + high) / 2;
         while (low <= high) {
-            int mid = (low + high) / 2;
+            mid = (low + high) / 2;
 
-            if (target > nums[mid])
-                low = mid + 1;
-            else
+            if (target < nums[mid]) //target落在左半区间
                 high = mid - 1;
+            else if (target > nums[mid]) //target落在右半区间
+                low = mid + 1;
+            else //target命中
+                return mid;
         }
-
-        /*
-        (1) At this point, low > high. That is, low >= high+1
-        (2) From the invariant, we know that the index is between [low, high+1], 
-        so low <= high+1. Follwing from (1), now we know low == high+1.
-        (3) Following from (2), the index is between [low, high+1] = [low, low], 
-        which means that low is the desired index
-
-        Therefore, we return low as the answer. 
-        You can also return high+1 as the result, since low == high+1
-        */
         return low;
     }
 }
